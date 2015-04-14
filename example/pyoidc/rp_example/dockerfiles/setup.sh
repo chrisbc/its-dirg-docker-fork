fd548abce02725ead025f0a2c7dd6806aa3d552a
Mathias Hedström <mathias.hedstrom.mh@gmail.com>
2015-04-14 16:06:06 +0200

    Added example files and run scripts for oidc

---
+++ example/pyoidc/rp_example/dockerfiles/setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+apt-get update
+apt-get install -y \
+        wget \
+        unzip
+
+# Create a folder for the pyoidc src
+mkdir /opt/dirg/src
+cd /opt/dirg/src
+
+# Download and unzip pyoidc
+wget "https://github.com/its-dirg/pyoidc/zipball/baseurl_command_param"
+zip_name=$(ls)
+unzip ${zip_name}
+rm ${zip_name}
+src_name=$(ls)
+mv ${src_name} pyoidc-master
+
+# install oidc_example/rp3 dependencies
+pip install cherrypy==3.2.4
+pip install -e /opt/dirg/src/pyoidc-master/
